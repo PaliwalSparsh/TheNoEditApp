976cc0c9343170b47115f264998e90cbb5affb3c
Sparsh Paliwal <sparshpailwal123@gmail.com>
2023-03-17 14:44:55 -0400

    Initial Commit

---
+++ theNoEditApp/theNoEditAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  theNoEditAppApp.swift
+//  theNoEditApp
+//
+//  Created by Sparsh Paliwal on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct theNoEditAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
