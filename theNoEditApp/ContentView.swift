1c08b06456af22d6d1e9e119599a87af77d42ceb
Sparsh Paliwal <sparshpailwal123@gmail.com>
2023-03-17 15:22:25 -0400

    Import images and scale grid

--- theNoEditApp/ContentView.swift
+++ theNoEditApp/ContentView.swift
@@ -1,21 +1,58 @@
-//
-//  ContentView.swift
-//  theNoEditApp
-//
-//  Created by Sparsh Paliwal on 3/17/23.
-//
-
 import SwiftUI
+import AppKit
 
 struct ContentView: View {
+    @State private var images = [NSImage]()
+    @State private var imageIDs = [NSImage: UUID]()
+    @State private var selectedImages = Set<UUID>()
+    @State private var gridSize: CGFloat = 100
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: gridSize))]) {
+                    ForEach(images, id: \.self) { image in
+                        Image(nsImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: gridSize, height: gridSize)
+                            .onTapGesture {
+                                if let id = imageIDs[image] {
+                                    if selectedImages.contains(id) {
+                                        selectedImages.remove(id)
+                                    } else {
+                                        selectedImages.insert(id)
+                                    }
+                                }
+                            }
+                            .border(selectedImages.contains(imageIDs[image]!) ? Color.blue : Color.clear)
+                    }
+                }
+            }
+            
+            HStack(spacing: 20) {
+                Button("Import Images") {
+                    let panel = NSOpenPanel()
+                    panel.allowsMultipleSelection = true
+                    panel.canChooseDirectories = true
+                    panel.begin { response in
+                        if response == NSApplication.ModalResponse.OK {
+                            for url in panel.urls {
+                                if let image = NSImage(contentsOf: url) {
+                                    images.append(image)
+                                    imageIDs[image] = UUID()
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                Slider(value: $gridSize, in: CGFloat(100)...CGFloat(600), label: {
+                    Text("Size")
+                }).frame(maxWidth: 400)
+
+            }.padding()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
