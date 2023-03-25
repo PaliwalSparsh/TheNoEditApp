34e3bdc85a1373b807ca6d9d3979b69795250d84
Sparsh Paliwal <sparshpailwal123@gmail.com>
2023-03-25 19:39:05 -0400

    Add logic to downsample and add loading indicator

--- theNoEditApp/ContentView.swift
+++ theNoEditApp/ContentView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import AppKit
+import UniformTypeIdentifiers
 
 struct ContentView: View {
     @State private var images = [NSImage]()
@@ -8,6 +9,7 @@ struct ContentView: View {
     @State private var gridSize: CGFloat = 100
     @State private var maxGridSize: CGFloat = 1200
     @State private var showSquareImages: Bool = false
+    @State private var isImportingImages: Bool = true
     
     var body: some View {
         GeometryReader { geo in
@@ -30,34 +32,19 @@ struct ContentView: View {
                                 .toggleStyle(.button)
                         }
                         
-                        
                         Spacer()
                         
-                        Button(action: {
-                            let panel = NSOpenPanel()
-                            panel.allowsMultipleSelection = true
-                            panel.canChooseDirectories = true
-                            panel.begin { response in
-                                if response == NSApplication.ModalResponse.OK {
-                                    for url in panel.urls {
-                                        if let image = NSImage(contentsOf: url) {
-                                            images.append(image)
-                                            imageIDs[image] = UUID()
-                                        }
-                                    }
-                                }
-                            }
-                        }) {
+                        Button(action: handleImport) {
                             Text("Import Images")
                                 .frame(maxWidth: 160)
                         }
-                        
                     }
                     .frame(maxWidth: .infinity)
                     .padding(16)
                     .background(.thinMaterial)
                     Divider()
                 }
+
                 // MARK: Image Grid
                 ScrollView {
                     LazyVGrid(columns: [GridItem(.adaptive(minimum: gridSize), spacing: 0)], spacing: 32) {
@@ -92,9 +79,6 @@ struct ContentView: View {
                     }
                 }
                 .onAppear {
-                    
-                }
-                .onAppear {
                     maxGridSize = geo.size.height - 18
                 }
                 .onChange(of: geo.size.height) { height in
@@ -102,10 +86,62 @@ struct ContentView: View {
                     maxGridSize = geo.size.height - 18
                     gridSize = ratio * maxGridSize
                 }
+                .overlay {
+                    if(isImportingImages) {
+                        ProgressView()
+                    }else{
+                        EmptyView()
+                    }
+                }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .animation(.easeInOut, value: showSquareImages)
+    }
+    
+    private func handleImport() {
+        isImportingImages = true
+        let panel = NSOpenPanel()
+        panel.allowedContentTypes = [.png, .jpeg, .rawImage, .tiff, .heif, .heic]
+        panel.allowsMultipleSelection = true
+        panel.canChooseDirectories = true
+        panel.begin { response in
+            if response == NSApplication.ModalResponse.OK {
+                for url in panel.urls {
+                    
+                    let image = downsample(imageAt: url, to: CGSize(width: 400, height: 400), scale: 1)
+                    images.append(image)
+                    imageIDs[image] = UUID()
+                    
+                    // MARK: This set of code was meant to be used if users selects a directory instead of individual images
+                    // do {
+                    //     let urlContentType = try url.resourceValues(forKeys: [.contentTypeKey]).contentType
+                    //     if (panel.allowedContentTypes.contains(urlContentType!)) {
+                    //         let image = downsample(imageAt: url, to: CGSize(width: 400, height: 400), scale: 1)
+                    //         images.append(image)
+                    //         imageIDs[image] = UUID()
+                    //     }
+                    // } catch {
+                    //     print("Caught an error")
+                    // }
+                }
+            }
+        }
+        isImportingImages = false
+    }
+    
+    private func downsample(imageAt imageURL: URL, to pointSize: CGSize, scale: CGFloat) -> NSImage {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
+        
+        let maxDimentionInPixels = max(pointSize.width, pointSize.height) * scale
+        
+        let downsampledOptions = [kCGImageSourceCreateThumbnailFromImageAlways: true,
+                                          kCGImageSourceShouldCacheImmediately: true,
+                                    kCGImageSourceCreateThumbnailWithTransform: true,
+                                           kCGImageSourceThumbnailMaxPixelSize: maxDimentionInPixels] as CFDictionary
+        let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampledOptions)!
+        return NSImage(cgImage: downsampledImage, size: NSSize(width: downsampledImage.width, height: downsampledImage.height))
     }
 }
 
