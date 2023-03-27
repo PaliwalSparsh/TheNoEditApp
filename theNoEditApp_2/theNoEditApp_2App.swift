5f545f10e445d4040fd3ffde0b2705de1f32f046
Sparsh Paliwal <sparshpailwal123@gmail.com>
2023-03-27 18:59:51 -0400

    Change deployment to mac

--- theNoEditApp_2/theNoEditApp_2App.swift
+++ theNoEditApp_2/theNoEditApp_2App.swift
@@ -1,14 +1,14 @@
 //
-//  theNoEditAppApp.swift
-//  theNoEditApp
+//  theNoEditApp_2App.swift
+//  theNoEditApp_2
 //
-//  Created by Sparsh Paliwal on 3/17/23.
+//  Created by Sparsh Paliwal on 3/27/23.
 //
 
 import SwiftUI
 
 @main
-struct theNoEditAppApp: App {
+struct theNoEditApp_2App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
