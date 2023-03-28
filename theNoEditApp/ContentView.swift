df4448e1abf8ab9b5b5515c545ab35daece4b4b1
Sparsh Paliwal <sparshpailwal123@gmail.com>
2023-03-28 16:20:21 -0400

    Use quicklook thumbnails for faster thumbnail generation

--- theNoEditApp/ContentView.swift
+++ theNoEditApp/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import AppKit
 import UniformTypeIdentifiers
+import QuickLookThumbnailing
 
 let MINIMUM_POSSIBLE_SIZE_OF_GRID: CGFloat = 600
 
@@ -16,7 +17,6 @@ struct ContentView: View {
     @State private var gridSize: CGFloat = MINIMUM_POSSIBLE_SIZE_OF_GRID
     @State private var maxGridSize: CGFloat = 1200
     @State private var showSquareImages: Bool = false
-    @State private var isImportingImages: Bool = false
     
     var body: some View {
         GeometryReader { geo in
@@ -40,14 +40,12 @@ struct ContentView: View {
                         }
                         
                         Spacer()
-                        
-                        // ShareLink("Share selected photos", items: images.filter{selectedImages.contains($0.id)}.map{NSImage(contentsOf: $0.url)})
-                        ShareLink(items: images.filter{selectedImages.contains($0.id)}.map{$0.url})
-                        
                         Button(action: handleImport) {
                             Text("Import Images")
                                 .frame(maxWidth: 120)
-                        }.disabled(isImportingImages)
+                        }
+                        
+                        ShareLink(items: images.filter{selectedImages.contains($0.id)}.map{$0.url})
                     }
                     .frame(maxWidth: .infinity)
                     .padding(16)
@@ -55,21 +53,10 @@ struct ContentView: View {
                     Divider()
                 }
                 
-                if isImportingImages || selectedImages.count > 0 {
-                    VStack(alignment: .leading, spacing: 0) {
-                        HStack(alignment: .center) {
-                            if(isImportingImages) {
-                                HStack() {
-                                    ProgressView().scaleEffect(0.5)
-                                    Text("Importing images...")
-                                }
-                            }
-                            Spacer()
-                            if(selectedImages.count > 0) {
-                                HStack(spacing:0) {
-                                    Text("**\(selectedImages.count)** images select")
-                                }
-                            }
+                if(selectedImages.count > 0) {
+                    VStack(spacing: 0) {
+                        HStack(alignment: .center, spacing:0) {
+                            Text("**\(selectedImages.count)** images select")
                         }
                         .frame(maxWidth: .infinity, maxHeight: 40)
                         .background(.ultraThickMaterial)
@@ -77,6 +64,7 @@ struct ContentView: View {
                         Divider()
                     }
                 }
+                
                 
                 // MARK: Image Grid
                 ScrollView {
@@ -131,50 +119,20 @@ struct ContentView: View {
         panel.allowsMultipleSelection = true
         panel.canChooseDirectories = true
         panel.begin { response in
-            isImportingImages = true
-            // Here we have 1 whole task responsible for importing all photos, we can also try creating Tasks for each import.
-            Task.detached {
-                if response == NSApplication.ModalResponse.OK {
-                    for url in await panel.urls {
-                        let data = downsample(imageAt: url, to: CGSize(width: 800, height: 800), scale: 1)
-                        
-                        // Sending the following operation to main thread to refresh the UI.
-                        DispatchQueue.main.async {
-                            images.append(ImageModel(url: url, data: data))
-                        }
-                    }
+            if response == NSApplication.ModalResponse.OK {
+                for url in panel.urls {
+                    // Use quicklook thumbnailing framework to generate thumbnails, much faster than other way to doing things
+                    let request = QLThumbnailGenerator.Request(fileAt: url, size: CGSize(width: 800, height: 800), scale: 1, representationTypes: .thumbnail)
                     
-                    // MARK: This set of code was meant to be used if users selects a directory instead of individual images
-                    // do {
-                    //     let urlContentType = try url.resourceValues(forKeys: [.contentTypeKey]).contentType
-                    //     if (panel.allowedContentTypes.contains(urlContentType!)) {
-                    //         let image = downsample(imageAt: url, to: CGSize(width: 400, height: 400), scale: 1)
-                    //         images.append(image)
-                    //         imageIDs[image] = UUID()
-                    //     }
-                    // } catch {
-                    //     print("Caught an error")
-                    // }
-                    DispatchQueue.main.async {
-                        isImportingImages = false
+                    QLThumbnailGenerator.shared.generateRepresentations(for: request) {
+                        thumbnail, representation, error in
+                        if let data = thumbnail {
+                            images.append(ImageModel(url: url, data: data.nsImage))
+                        }
                     }
                 }
             }
         }
-    }
-    
-    private func downsample(imageAt imageURL: URL, to pointSize: CGSize, scale: CGFloat) -> NSImage {
-        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
-        let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, imageSourceOptions)!
-        
-        let maxDimentionInPixels = max(pointSize.width, pointSize.height) * scale
-        
-        let downsampledOptions = [kCGImageSourceCreateThumbnailFromImageAlways: true,
-                                          kCGImageSourceShouldCacheImmediately: true,
-                                    kCGImageSourceCreateThumbnailWithTransform: true,
-                                           kCGImageSourceThumbnailMaxPixelSize: maxDimentionInPixels] as CFDictionary
-        let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampledOptions)!
-        return NSImage(cgImage: downsampledImage, size: NSSize(width: downsampledImage.width, height: downsampledImage.height))
     }
 }
 
